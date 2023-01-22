b1b07d0a65fdfc7db1122d5a43bc23f961ada953
bobfishkins23 <bobfishkins23@yahoo.com>
2023-01-21 23:33:15 -0500

    Timer tune

--- Content/Buildings/Building.cs
+++ Content/Buildings/Building.cs
@@ -209,7 +209,7 @@ namespace BrickAndMortar.Content.Buildings
 
 			spriteBatch.Draw(tex, position, source, lightColor);
 
-			DrawBuildTime(spriteBatch, position + new Vector2(Width * 16 / 2 - 42, -32));
+			DrawBuildTime(spriteBatch, position + new Vector2(Width * 16 / 2 - 42, -36));
 		}
 
 		/// <summary>
