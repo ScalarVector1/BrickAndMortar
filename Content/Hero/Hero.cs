e2ed03c6ededbfc3e11d7a5bc819f81f74fc5f46
bobfishkins23 <bobfishkins23@yahoo.com>
2023-01-24 21:30:15 -0500

    Hero architecture

---
+++ Content/Hero/Hero.cs
@@ -0,0 +1,103 @@
+﻿using BrickAndMortar.Content.Buildings;
+using System;
+using Terraria.ModLoader.IO;
+
+namespace BrickAndMortar.Content.Hero
+{
+	internal abstract class Hero : ModNPC
+	{
+		public Building parent;
+
+		public Player following;
+
+		public HeroAttack primaryAttack;
+		public HeroAttack secondaryAttack;
+		public HeroAttack activeAttack;
+
+		/// <summary>
+		/// Global timer for this hero. Will be ticked automaticall.
+		/// </summary>
+		public ref float Timer => ref NPC.ai[0];
+		/// <summary>
+		/// Timer passed to attacks for use. Will be reset when an attack returns true from Activate.
+		/// </summary>
+		public ref float AttackTimer => ref NPC.ai[1];
+		/// <summary>
+		/// Global state for the hero. Can be used to track things like Follow VS Wander VS Attack.
+		/// </summary>
+		public ref float State => ref NPC.ai[2];
+		/// <summary>
+		/// Which attack the hero should be using. 0 = primary, 1 = secondary, 2 = active, -1 = nothing
+		/// </summary>
+		public ref float AttackState => ref NPC.ai[3];
+
+		public int Level => parent.level;
+
+		public int EffectiveLevel => parent.EffectiveLevel;
+
+		public sealed override void SetDefaults()
+		{
+			NPC.friendly = true;
+			NPC.dontCountMe = true;
+			NPC.lifeMax = 20;
+			NPC.damage = 1;
+			SafeSetDefaults();
+			RefreshStats();
+		}
+
+		/// <summary>
+		/// Set things like width/height here only. Use RefreshStats to set things such as like and damage based on leveling.
+		/// </summary>
+		public virtual void SafeSetDefaults() { }
+
+		/// <summary>
+		/// Where you should set stats like life, damage, etc. based on EffectiveLevel. To be called on leveling the altar and creation.
+		/// </summary>
+		public virtual void RefreshStats() { }
+
+		public sealed override void AI()
+		{
+			Timer++;
+			AttackTimer++;
+
+			bool shouldReset = AttackState switch
+			{
+				0 => primaryAttack.Activate((int)AttackTimer),
+				1 => secondaryAttack.Activate((int)AttackTimer),
+				2 => activeAttack.Activate((int)AttackTimer),
+				_ => true
+			};
+
+			if (shouldReset)
+			{
+				AttackTimer = 0;
+				AttackState = -1;
+			}
+		}
+
+		/// <summary>
+		/// This is the primary method you should be overriding for general behavior like movement. Setting AttackState here will determine what happens when AI runs. You can
+		/// check for the hero being ready for a new attack by checking if AttackState == -1. Note that this will run before Timer and AttackTimer updates for that frame.
+		/// </summary>
+		/// <returns>If default AI should run (using attack based on AttackState, resetting AttackTimer and AttackState if attack is over, incrementing Timer and AttackTimer)</returns>
+		public override bool PreAI()
+		{
+			return base.PreAI(); //Yes, I overrode this just for documentation.
+		}
+
+		//Save and load utility to remember which abilities this hero has. To be saved/loaded by the altar building's save/load methods.
+		public void SaveDataToBuilding(TagCompound tag)
+		{
+			tag["primary"] = primaryAttack.GetType().ToString();
+			tag["secondary"] = primaryAttack.GetType().ToString();
+			tag["active"] = primaryAttack.GetType().ToString();
+		}
+
+		public void LoadDataFromBuilding(TagCompound tag)
+		{
+			primaryAttack = (HeroAttack)Activator.CreateInstance(BrickAndMortar.instance.Code.GetType(tag.GetString("primary")));
+			secondaryAttack = (HeroAttack)Activator.CreateInstance(BrickAndMortar.instance.Code.GetType(tag.GetString("secondary")));
+			activeAttack = (HeroAttack)Activator.CreateInstance(BrickAndMortar.instance.Code.GetType(tag.GetString("active")));
+		}
+	}
+}
---
+++ Content/Hero/HeroAttack.cs
@@ -0,0 +1,29 @@
+﻿using BrickAndMortar.Content.Buildings;
+
+namespace BrickAndMortar.Content.Hero
+{
+	internal abstract class HeroAttack
+	{
+		/// <summary>
+		/// The hero who owns this attack instance
+		/// </summary>
+		public Hero owner;
+
+		/// <summary>
+		/// The stat line that this attack should show on the altar building UI when it's selected
+		/// </summary>
+		public abstract Statline statline { get; }
+
+		/// <summary>
+		/// The stat line this attack should show on the altar building UI when it's selected and about to be upgraded
+		/// </summary>
+		public abstract Statline statlineNext { get; }
+
+		/// <summary>
+		/// The behavior of the actual attack when activated. Spawning projectiles, damaging enemies, etc.
+		/// </summary>
+		/// <param name="time">The attack timer of the hero</param>
+		/// <returns>if the attack is 'over' and the hero should reset it's attack state.</returns>
+		public abstract bool Activate(int time);
+	}
+}
